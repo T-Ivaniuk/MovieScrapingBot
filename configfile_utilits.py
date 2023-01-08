d624423b493ac0ec28cfe1551b02f48cc492906e
T-Ivaniuk <taras.ivaniuk98@gmail.com>
2023-01-08 22:53:25 +0200

    add file to manage bot settings

---
+++ configfile_utilits.py
@@ -0,0 +1,8 @@
+telegramtoken = "TG TOKEN"
+gmail_sender = "GMAIL LOGIN"
+gmail_password = "GMAIL PASSWORD"
+telegramwhitelist = {
+    10000001: "user@gmail.com",  # telegram_user_id : "user email"
+}
+
+folder_for_tg = "tg_files/"
